5551d727a439e7b2e36fe0fca14e0e99bf243c92
Levashan <LevashanPillay11@gmail.com>
2023-11-27 17:50:47 +0200

    Virtual Camera follow

--- Multiplayer_Base/Assets/Scripts/General/RpcTest.cs
+++ Multiplayer_Base/Assets/Scripts/General/RpcTest.cs
@@ -1,13 +1,27 @@
 using Unity.Netcode;
 using UnityEngine;
+using Cinemachine;
 
 public class RpcTest : NetworkBehaviour
 {
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private AudioListener audioListener;
+
     public override void OnNetworkSpawn()
     {
         if (!IsServer && IsOwner) //Only send an RPC to the server on the client that owns the NetworkObject that owns this NetworkBehaviour instance
         {
             TestServerRpc(0, NetworkObjectId);
+        }
+
+        if (IsOwner)
+        {
+            audioListener.enabled = true;
+            virtualCamera.Priority = 1;
+        }
+        else
+        {
+            virtualCamera.Priority = 0;
         }
     }
 
